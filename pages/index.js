b038987374562430e2d3718803bf4c24529af2e2
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-12-21 14:41:07 -0500

    Add text color palette

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="p-6 max-w-sm mx-auto bg-blue-light rounded-xl shadow-md flex items-center space-x-4">
       <div className="flex-shrink-0">
       </div>
       <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
+        <div className="text-xl font-medium text-text-white">ChitChat</div>
+        <p className="text-text-white">You have a new message!</p>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        lightest: '#00a8cc',
+        light: '#0c7b93',
+        dark: '#27496d',
+        darker: '#142850'
+      },
+      text: {
+        white: '#f7f7f7',
+        bone: '#eeeeee',
+        black: '#393e46',
+        gray: '#929aab'
+      }
+    },
   },
   variants: {
     extend: {},
