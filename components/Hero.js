832662e1d914e083946f1d82927e3763b811a3b8
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-12-21 22:16:10 -0500

    Change to global.css

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import Header from './Header';
 export default function hero() {
     return (
-        <div className="container mx-auto bg-text-white">
+        <div className="container mx-auto bg-text-white border-text-black">
             <div className="relative bg-white overflow-hidden">
                 <div className="max-w-7xl mx-auto">
                     <div className="relative z-10 pb-8 bg-text-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
