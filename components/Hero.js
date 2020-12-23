b573a5a9697d8ea8439d2db8267c08b9325545b9
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-12-23 17:20:40 -0500

    Animate hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import { motion } from 'framer-motion'
 export default function hero() {
     return (
         <div className="container mx-auto bg-text-white border-text-black">
@@ -9,26 +10,44 @@ export default function hero() {
                             <polygon points="50,0 100,0 50,100 0,100" />
                         </svg>
                         <Header />
-                        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                            <div className="sm:text-center lg:text-left">
-                                <h1 className="text-4xl tracking-tight font-Poppins text-text-black sm:text-5xl md:text-6xl">
-                                    <span className="block xl:inline">Hello I'm</span>
-                                </h1>
-                                <h1 className="text-4xl tracking-tight font-Poppins text-blue-lightest sm:text-5xl md:text-6xl">
-                                    <span className="block xl:inline">Ali Sequeira</span>
-                                </h1>
-                                <p className="mt-3 text-base text-text-black sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-Lato">
-                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                             </p>
-                                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                                    <div className="rounded-md shadow">
-                                        <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-Puppins rounded-md text-text-white bg-blue-lightest hover:bg-blue-dark hover:text-text-white md:py-4 md:text-lg md:px-10">
-                                            Get started
+                        <motion.div
+                            initial="hidden"
+                            animate="visible"
+                            variants={{
+                                hidden: {
+                                    scale: .8,
+                                    opacity: 0
+                                },
+                                visible: {
+                                    scale: 1,
+                                    opacity: 1,
+                                    transition: {
+                                        delay: .4
+                                    }
+                                }
+                            }}
+                        >
+                            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                                <div className="sm:text-center lg:text-left">
+                                    <h1 className="text-4xl tracking-tight font-Poppins text-text-black sm:text-5xl md:text-6xl">
+                                        <span className="block xl:inline">Hello I'm</span>
+                                    </h1>
+                                    <h1 className="text-4xl tracking-tight font-Poppins text-blue-lightest sm:text-5xl md:text-6xl">
+                                        <span className="block xl:inline">Ali Sequeira</span>
+                                    </h1>
+                                    <p className="mt-3 text-base text-text-black sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-Lato">
+                                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                    </p>
+                                    <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                        <div className="rounded-md shadow">
+                                            <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-Puppins rounded-md text-text-white bg-blue-lightest hover:bg-blue-dark hover:text-text-white md:py-4 md:text-lg md:px-10">
+                                                Get started
                                     </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </main>
+                            </main>
+                        </motion.div>
                     </div>
                 </div>
                 <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
