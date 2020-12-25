7de1ac056c4e382a0c28a6d0b179c75b67993c14
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-12-25 15:24:24 -0500

    .

--- components/Header.js
+++ components/Header.js
@@ -33,9 +33,6 @@ export default function Header() {
             <div className={open ? "absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" : "absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden hidden"}>
                 <div className="rounded-lg shadow-md bg-text-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div className="px-5 pt-4 flex items-center justify-between">
-                        <div>
-                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
-                        </div>
                         <div className="-mr-2">
                             <button type=" button" onClick={() => setOpen(!open)} className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                 <span className="sr-only">Close main menu</span>
