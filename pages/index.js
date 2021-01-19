c5298659afb1def6fc740a91df341e6c9f6a41c2
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-01-19 13:24:15 -0500

    Add Contact Form

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Footer from '../components/Footer'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </Layout>
   )
